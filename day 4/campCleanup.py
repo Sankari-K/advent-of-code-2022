81e08df3626058183eca25e603f3acbca400e5dd
Sankari Karthik <sankarikarthik03#gmail.com>
2022-12-04 10:55:17 +0530

    Update solution for day 4

---
+++ day 4/campCleanup.py
@@ -0,0 +1,49 @@
+def day4_parta():
+    pairs = 0
+    while True:
+        inp = input()
+        if inp == "q":
+            break
+        first, second = inp.split(",")
+
+        first = getRange(first)
+        second = getRange(second)
+        
+        if second[0] >= first[0] and second[1] <= first[1]:
+            pairs += 1
+        elif first[0] >= second[0] and first[1] <= second[1]:
+            pairs += 1
+        
+    return pairs
+
+def day4_partb():
+    pairs = 0
+    while True:
+        inp = input()
+        if inp == "q":
+            break
+        first, second = inp.split(",")
+
+        first = getRange(first)
+        second = getRange(second)
+        
+        if first[1] < second[0] or second[1] < first[0]:
+            pass
+        else:
+            pairs += 1
+        
+    return pairs
+
+def getRange(interval):
+    start, stop = map(int, interval.split("-"))
+    return [start, stop]
+
+"""
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+"""
+print(day4_partb())
