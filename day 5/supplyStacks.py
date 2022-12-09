44f82db6195d9dd57cd731f51b3058faf7de6829
Sankari Karthik <sankarikarthik03#gmail.com>
2022-12-09 17:15:28 +0530

    Make a parser

--- day 5/supplyStacks.py
+++ day 5/supplyStacks.py
@@ -1,26 +1,41 @@
-def day5_parta():
-    crates = {1: ["B", "L", "D", "T", "W", "C", "F", "M"],
-              2: ["N", "B", "L"],
-              3: ["J", "C", "H", "T", "L", "V"],
-              4: ["S", "P", "J", "W"],
-              5: ["Z", "S", "C", "F", "T", "L", "R"],
-              6: ["W", "D", "G", "B", "H", "N", "Z"],
-              7: ["F", "M", "S", "P", "V", "G", "C", "N"],
-              8: ["W", "Q", "R", "J", "F", "V", "C", "Z"],
-              9: ["R", "P", "M", "L", "H"]}
+import copy 
 
-    # crates = {1: ["Z", "N"],
-    #           2: ["M", "C", "D"],
-    #           3: ["P"]}
-
-    while True:
+def stackParser():
+    # get line by line and add to a 2d list
+    stacks = []
+    inp = input()
+    while inp:
+        stacks.append(inp)
         inp = input()
-        if inp == "q":
-            break
+    # transpose
+    stacks = list(zip(*stacks))
+
+    # remove noise
+    new_stack = []
+    for line in stacks:
+        new_line = list(filter(lambda x: x not in "[ ]", line))
+        if new_line:
+            new_stack.append("".join(new_line))
+
+    # create a dict
+    stacks = dict()
+    for stack in new_stack:
+        stacks[int(stack[-1])] = list(stack[:-1][::-1])
+    return stacks
+
+def instructionParser():
+    instructions = []
+    inp = input()
+    while inp:
         _, qty, _, source, _, destination = inp.split()
-
         qty, source, destination = map(int, [qty, source, destination])
-
+        instructions.append([qty, source, destination])
+        inp = input()
+    return instructions
+    
+def day5_parta(crates, instructions):
+    for instruction in instructions:
+        qty, source, destination = instruction
         for _ in range(qty):
             crates[destination].append(crates[source].pop())
 
@@ -29,35 +44,20 @@ def day5_parta():
         top += value[-1]
     return top
 
-def day5_partb():
-    crates = {1: ["B", "L", "D", "T", "W", "C", "F", "M"],
-              2: ["N", "B", "L"],
-              3: ["J", "C", "H", "T", "L", "V"],
-              4: ["S", "P", "J", "W"],
-              5: ["Z", "S", "C", "F", "T", "L", "R"],
-              6: ["W", "D", "G", "B", "H", "N", "Z"],
-              7: ["F", "M", "S", "P", "V", "G", "C", "N"],
-              8: ["W", "Q", "R", "J", "F", "V", "C", "Z"],
-              9: ["R", "P", "M", "L", "H"]}
-
-    # crates = {1: ["Z", "N"],
-    #           2: ["M", "C", "D"],
-    #           3: ["P"]}
-
-    while True:
-        inp = input()
-        if inp == "q":
-            break
-        _, qty, _, source, _, destination = inp.split()
-
-        qty, source, destination = map(int, [qty, source, destination])
-
+def day5_partb(crates, instructions):
+    for instruction in instructions:
+        qty, source, destination = instruction
         crates[destination] += crates[source][-1 * qty:]
         crates[source] = crates[source][: -1 * qty]
-
+    
     top = ""
     for value in crates.values():
         top += value[-1]
     return top
 
-print(day5_parta())
+crates = stackParser()
+new_crates = copy.deepcopy(crates)
+instructions = instructionParser()
+
+print(day5_parta(crates, instructions))
+print(day5_partb(new_crates, instructions))
