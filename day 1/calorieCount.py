b8e7ca19bff656385e075e55a0e8c9d2f83943d4
Sankari Karthik <sankarikarthik03#gmail.com>
2022-12-09 15:52:26 +0530

    Include inputs

--- day 1/calorieCount.py
+++ day 1/calorieCount.py
@@ -1,13 +1,13 @@
-def day1():
-    calories = [0]
-    while True:
-        calorie = input()
-        if calorie == "q":
-            break
-        if calorie == "":
-            calories.append(0)
-        else:
-            calories[-1] += int(calorie)
-        
-    calories = sorted(calories, reverse=True)
-    return sum(calories[0:3])
+calories = [0]
+while True:
+    calorie = input()
+    if calorie == "q":
+        break
+    if calorie == "":
+        calories.append(0)
+    else:
+        calories[-1] += int(calorie)
+    
+calories = sorted(calories, reverse=True)
+print(sum(calories[0])) # part a
+print(sum(calories[0:3])) # part b
